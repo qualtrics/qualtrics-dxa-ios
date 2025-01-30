bb4ddffc7e669ae469990bec9f22a05076780422
Piotr Opyd <popyd@qualtrics.com>
2025-01-30 11:44:01 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "QualtricsDXA",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        
+    ],
+    targets: [
+        
+    ]
+)
