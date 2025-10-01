462c3a02a2c3609ff934d4fac01dcd1b5aedd7ac
Bitrise CI <noreply@bitrise.io>
2025-10-01 07:54:37 +0000

    Release 0.4.2

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "QualtricsDXA",
-            url: "https://s3-us-west-2.amazonaws.com/si-mobile-sdks/dxa/ios/0.4.1/QualtricsDXA.zip",
-            checksum: "1dd28ad7b4657939a9c62c2d0526f1a91b2c63e82328ff385238552984b703a9"
+            url: "https://s3-us-west-2.amazonaws.com/si-mobile-sdks/dxa/ios/0.4.2/QualtricsDXA.zip",
+            checksum: "6d575c463dd5d4ae95134552ef380bb62f015e016a7ce5e5f7e96a6293443108"
         )
     ]
 )
